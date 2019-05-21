c1afe32fe071968226ddea0240a6a341ff90f9e6
np <np@DESKTOP-8FM5MJT>
2019-05-21 21:24:24 +0800

    接受微信公众号

--- src/main/java/org/jkjava/weixin/controller/MessageReceiverControll.java
+++ src/main/java/org/jkjava/weixin/controller/MessageReceiverControll.java
@@ -1,26 +1,42 @@
 package org.jkjava.weixin.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/kemao_1/wexin/receiver")
 public class MessageReceiverControll {
-
+	private static final Logger LOG =LoggerFactory.getLogger(MessageReceiverControll.class);
 	@GetMapping
 	public  String echo(
-			String signature,
-			String timestamp,
-			String nonce,
-			String echostr
-			
-			
+			@RequestParam("signature") String signature,
+			@RequestParam("timestamp") String timestamp,
+			@RequestParam("nonce") String nonce,
+			@RequestParam("echostr") String echostr
 			) {
-		
-		
-		
 		return echostr;
 	}
+	
+	@PostMapping
+	 
+	public String onMessage(
+			@RequestParam("signature") String signature,
+			@RequestParam("timestamp") String timestamp,
+			@RequestParam("nonce") String nonce,
+			@RequestBody String xml) {
+			//收到消息
+		LOG.trace("收到的消息原文:\n{}\n--------------",xml);
+		
+		
+		return null;
+		
+	}
+	
 }
